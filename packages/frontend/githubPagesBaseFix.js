44d4a88ba9a9a278ccdaa9d72d6901fdd5979491
Max Fisher <maxmfishernj@gmail.com>
2021-11-11 14:18:17 -0800

    add github workflow check to add base href

--- packages/frontend/githubPagesBaseFix.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-const indexPath = path.resolve(__dirname, './dist/index.html')
-const contents = fs.readFileSync(indexPath, 'utf-8')
-const replaced = contents.replace('<head>','<head>\n<base href="/polyratings-revamp/">')
-
-fs.writeFileSync(indexPath, replaced)
--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:process.env.GITHUB_WORKFLOW ? '/polyratings-revamp/' : '/',
   plugins: [react()]
 })
