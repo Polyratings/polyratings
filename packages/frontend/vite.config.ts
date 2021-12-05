c62ab0a6f887eafacce46bb1fd6149d6757a8a9b
Max Fisher <maxmfishernj@gmail.com>
2021-12-05 12:53:14 -0800

    added ability to analyze bundle

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const { visualizer } = require('rollup-plugin-visualizer');
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base:process.env.GITHUB_WORKFLOW ? '/polyratings-revamp/' : '/',
-  plugins: [react()]
+  plugins: [react()],
+  build:{
+    sourcemap:true,
+    // Enable for bundle stats
+    // rollupOptions:{
+    //   plugins:[
+    //     visualizer({
+    //       filename: resolve(__dirname, 'stats/stats.html'),
+    //       template: 'treemap', // sunburst|treemap|network
+    //       sourcemap: true
+    //   })]
+    // }
+  }
 })
