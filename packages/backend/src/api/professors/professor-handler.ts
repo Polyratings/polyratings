211203a0fa0476d8c7b8219329e43cfcf2474afc
Addison Tustin <addisontustin@gmail.com>
2022-02-06 02:25:06 -0800

    Refactor ProfessorHandler to use KVDAO

--- packages/backend/src/api/professors/professor-handler.ts
+++ packages/backend/src/api/professors/professor-handler.ts
@@ -1,31 +1,24 @@
 import { Context } from 'sunder';
 import { Env } from '@polyratings/backend/bindings';
-import { ProfessorDTO } from '@polyratings/backend/dtos/Professors';
-import { plainToInstance } from 'class-transformer';
 import { DtoBypass } from '@polyratings/backend/dtos/DtoBypass';
-import { PolyratingsError } from '@polyratings/backend/utils/errors';
+import { KVDAO } from '@polyratings/backend/api/dao/kv-dao';
 
 export class ProfessorHandler {
     /**
      * Retrieves the entire list of professors from the KV store
-     * @param ctx - sunder routing context
      */
     static async getProfessorList(ctx: Context<Env>) {
-        const professorList = await ctx.env.POLYRATINGS.get('all');
-        if (professorList === null) {
-            throw new PolyratingsError(404, "No professors found!");
-        }
-        
-        // Use dto bypass because the professor list is too large to be used with class-transformer efficiently
-        // We also know that the data stored in `all` is safe to send
-        ctx.response.body = new DtoBypass(professorList)
+        const kv = new KVDAO(ctx);
+
+        ctx.response.body = new DtoBypass(await kv.getAllProfessors());
     }
 
+    /**
+     * Retrieves a single professor from the KV store
+     */
     static async getSingleProfessor(ctx: Context<Env, {id: string}>) {
-        const professor = await ctx.env.POLYRATINGS.get(ctx.params.id);
-        if (professor === null) {
-            throw new PolyratingsError(404, "Professor not found!")
-        }
-        ctx.response.body = plainToInstance(ProfessorDTO, JSON.parse(professor));
+        const kv = new KVDAO(ctx);
+
+        ctx.response.body = await kv.getProfessor(ctx.params.id);
     }
 }
