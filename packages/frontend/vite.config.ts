c63384600ff8d7c92492bfc8bec02a40cef4ffc0
mfish33 <maxmfishernj@gmail.com>
2022-02-18 12:09:42 -0800

    vite now will build shared umd

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,23 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-// import { resolve } from 'path'
-// const { visualizer } = require('rollup-plugin-visualizer');
+import { resolve } from 'path'
+
+const { visualizer } = require('rollup-plugin-visualizer');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_WORKFLOW ? '/polyratings-revamp/' : '/',
   plugins: [react(), tsconfigPaths()],
   build: {
+    // Needed since we are compiling shared as a UMD package to make jest happy :(
+    commonjsOptions: { exclude: ['@polyratings/shared'], include: [] },
     sourcemap: true,
-    // Enable for bundle stats
-    // rollupOptions:{
-    //   plugins:[
-    //     visualizer({
-    //       filename: resolve(__dirname, 'stats/stats.html'),
-    //       template: 'treemap', // sunburst|treemap|network
-    //       sourcemap: true
-    //   })]
-    // }
+    rollupOptions:{
+      plugins:[
+        visualizer({
+          filename: resolve(__dirname, 'stats/stats.html'),
+          template: 'treemap', // sunburst|treemap|network
+          sourcemap: true
+      })]
+    }
+  },
+  optimizeDeps: {
+    include: ['@polyratings/shared'],
   },
 });
