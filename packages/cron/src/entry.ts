7cb24bb5b90bad61b8093721bec4bcc34780461c
mfish33 <maxmfishernj@gmail.com>
2022-02-25 13:33:53 -0800

    added step to clear processing queues

--- packages/cron/src/entry.ts
+++ packages/cron/src/entry.ts
@@ -1,5 +1,6 @@
 import { Teacher } from "@polyratings/shared";
 import * as toml from "toml";
+import * as fs from "node:fs";
 import { syncProfessors } from "./steps/syncProfessors";
 import { cloudflareKVInit } from "./wrappers/kv-wrapper";
 import { Logger } from "./logger";
@@ -8,18 +9,29 @@ import { PolyratingsWorkerWrapper } from "./wrappers/worker-wrapper";
 // Typically this should be exported except it is a deployment file
 // eslint-disable-next-line import/no-relative-packages
 import backendDeployToml from "../../backend/wrangler.toml";
-
-async function executeOrPrintError(fn: () => Promise<void>) {
-    try {
-        await fn();
-    } catch (e) {
-        Logger.error(`Failed to run ${fn.name}\n`, `Got Error: ${(e as Error).toString()}`);
-    }
-}
+import { clearProcessingQueues } from "./steps/clearProcessingQueues";
 
 export async function main(env: Record<string, string>) {
-    const runtimeEnv = await createRuntimeEnvironment(env);
-    await executeOrPrintError(() => syncProfessors(runtimeEnv));
+    let runtimeEnv: CronEnv;
+    try {
+        runtimeEnv = await createRuntimeEnvironment(env);
+    } catch (e) {
+        Logger.error(`Failed to create cron runtime environment\nReceived Error: ${e}`);
+        return;
+    }
+
+    const tasks = [
+        { name: "syncProfessors", task: syncProfessors },
+        { name: "clearProcessingQueues", task: clearProcessingQueues },
+    ];
+
+    for (const { task, name } of tasks) {
+        try {
+            await task(runtimeEnv);
+        } catch (e) {
+            Logger.error(`Failed to run \`${name}\`\n`, `Got Error: ${(e as Error).toString()}`);
+        }
+    }
 }
 
 export interface CronEnv {
@@ -28,6 +40,8 @@ export interface CronEnv {
     prodWorkerUrl: string;
     betaProfessorKvId: string;
     devProfessorKvId: string;
+    reviewProcessingQueueId: string;
+    professorProcessingQueueId: string;
     polyratingsCIUsername: string;
     polyratingsCIPassword: string;
     KVWrapper: ReturnType<typeof cloudflareKVInit>;
@@ -48,6 +62,13 @@ async function createRuntimeEnvironment(globalEnv: Record<string, string>): Prom
     const devProfessorKvId = parsedToml.env.dev.kv_namespaces.find(
         (namespace: Record<string, unknown>) => namespace.binding === "POLYRATINGS_TEACHERS",
     )?.id;
+    const reviewProcessingQueueId = parsedToml.env.prod.kv_namespaces.find(
+        (namespace: Record<string, unknown>) => namespace.binding === "PROCESSING_QUEUE",
+    )?.id;
+    const professorProcessingQueueId = parsedToml.env.prod.kv_namespaces.find(
+        (namespace: Record<string, unknown>) =>
+            namespace.binding === "POLYRATINGS_TEACHER_APPROVAL_QUEUE",
+    )?.id;
 
     const polyratingsCIUsername = globalEnv.POLYRATINGS_CI_USERNAME;
     const polyratingsCIPassword = globalEnv.POLYRATINGS_CI_PASSWORD;
@@ -57,13 +78,16 @@ async function createRuntimeEnvironment(globalEnv: Record<string, string>): Prom
     await polyratingsProdWorker.login(polyratingsCIUsername, polyratingsCIPassword);
     const prodProfessorData = await polyratingsProdWorker.professorEntries();
     Logger.info(`Got ${prodProfessorData.length} professors from prod`);
+    fs.writeFileSync("./dump.json", JSON.stringify(prodProfessorData));
 
     const KVWrapper = cloudflareKVInit(globalEnv.CF_API_KEY, globalEnv.CF_EMAIL);
 
-    const out = {
+    const out: CronEnv = {
         accountId,
         betaProfessorKvId,
         devProfessorKvId,
+        reviewProcessingQueueId,
+        professorProcessingQueueId,
         prodWorkerUrl,
         polyratingsCIPassword,
         polyratingsCIUsername,
