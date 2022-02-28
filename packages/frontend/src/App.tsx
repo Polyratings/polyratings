0227a370b402de3568ed6f14d3f0da29f4c59ebc
mfish33 <maxmfishernj@gmail.com>
2022-02-27 23:32:02 -0800

    frontend logging

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,72 +2,53 @@ import { Router, Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { createBrowserHistory } from "history";
 import ReactGA from "react-ga4";
-import { Component, useEffect } from "react";
+import { useEffect } from "react";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import { Home, TeacherPage, Login, NewTeacher, About, SearchWrapper, Admin } from "./pages";
 import { Navbar } from "./components";
 import "react-toastify/dist/ReactToastify.css";
 import { config } from "./App.config";
-import { useService } from "./hooks";
-import { Logger } from "./services";
+
+const history = createBrowserHistory({ basename: config.base });
+const SentryRoute = Sentry.withSentryRouting(Route);
+
+Sentry.init({
+    dsn: "https://150f4ee898954b91aac4834cace32008@o1154721.ingest.sentry.io/6234908",
+    integrations: [
+        new BrowserTracing({
+            routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
+        }),
+    ],
+
+    // 20% of transactions get sent to Sentry
+    tracesSampleRate: 0.2,
+});
 
 function App() {
     useEffect(() => {
         ReactGA.initialize("G-784BKPF31W");
     }, []);
 
-    const history = createBrowserHistory({ basename: config.base });
     history.listen((location) => {
         ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
     });
     return (
-        <GlobalErrorBoundary>
+        <Sentry.ErrorBoundary showDialog>
             <Router history={history}>
                 <ToastContainer />
                 <Navbar />
                 <Switch>
-                    <Route path="/teacher/:id" component={TeacherPage} />
-                    <Route path="/search/:searchType" component={SearchWrapper} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/new-teacher" component={NewTeacher} />
-                    <Route path="/about" component={About} />
-                    <Route path="/admin" component={Admin} />
-                    <Route path="/" component={Home} />
+                    <SentryRoute path="/teacher/:id" component={TeacherPage} />
+                    <SentryRoute path="/search/:searchType" component={SearchWrapper} />
+                    <SentryRoute path="/login" component={Login} />
+                    <SentryRoute path="/new-teacher" component={NewTeacher} />
+                    <SentryRoute path="/about" component={About} />
+                    <SentryRoute path="/admin" component={Admin} />
+                    <SentryRoute path="/" component={Home} />
                 </Switch>
             </Router>
-        </GlobalErrorBoundary>
+        </Sentry.ErrorBoundary>
     );
 }
-
-class GlobalErrorBoundary extends Component {
-    componentDidMount() {
-        // Add an event listener to the window to catch unhandled promise rejections & stash the error in the state
-        window.addEventListener("unhandledrejection", this.promiseRejectionHandler);
-    }
-
-    componentDidCatch(error: Error) {
-        this.logError(error);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener("unhandledrejection", this.promiseRejectionHandler);
-    }
-
-    private promiseRejectionHandler = (event: PromiseRejectionEvent) => {
-        event.preventDefault();
-        this.logError(event.reason);
-    };
-
-    // eslint-disable-next-line class-methods-use-this
-    private logError(error: Error) {
-        const logger = useService(Logger);
-        const errorPlain = JSON.parse(JSON.stringify(error, Object.getOwnPropertyNames(error)));
-        logger.error(errorPlain);
-    }
-
-    render() {
-        const { children } = this.props;
-        return children;
-    }
-}
-
 export default App;
