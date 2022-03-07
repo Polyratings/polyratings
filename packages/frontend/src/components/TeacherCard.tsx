4abebc707b3cb66eabe14e0cd6ed862fb3f56b6e
Max Fisher <maxmfishernj@gmail.com>
2022-03-06 23:34:37 -0800

    allow new tab on teacher card

--- packages/frontend/src/components/TeacherCard.tsx
+++ packages/frontend/src/components/TeacherCard.tsx
@@ -1,5 +1,5 @@
 import { Teacher } from "@polyratings/shared";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import star from "@/assets/star.svg";
 import { useService } from "@/hooks";
 import { TeacherService } from "@/services";
@@ -15,7 +15,10 @@ export function TeacherCard({ teacher, beforeNavigation = () => {} }: TeacherCar
     const history = useHistory();
     const teacherService = useService(TeacherService);
 
-    const onClick = async () => {
+    const onClick = async (e: React.MouseEvent) => {
+        // Stop navigation from the parent <Link /> element
+        e.preventDefault();
+
         // await the pre-navigation handler passed into the component
         await Promise.resolve(beforeNavigation());
 
@@ -27,23 +30,26 @@ export function TeacherCard({ teacher, beforeNavigation = () => {} }: TeacherCar
     };
 
     return (
-        <div onClick={onClick}>
-            <div
-                className="w-full h-32 border-cal-poly-gold border-4 bg-white flex justify-between items-center text-cal-poly-green cursor-pointer"
-                style={{ borderRadius: "1.5rem" }}
-            >
-                <h3 className="text-3xl font-medium pl-3">
-                    {teacher?.lastName}, {teacher?.firstName}
-                </h3>
-                <div className="text-right text-xl pr-3 font-medium flex-shrink-0">
-                    <div>{teacher?.department}</div>
-                    <div className="flex items-center justify-end">
-                        <img className="pr-1 h-4" src={star} alt="" />
-                        <div>{teacher?.overallRating}</div>
+        <Link to={`/teacher/${teacher?.id}`}>
+            <div onClick={onClick}>
+                <div
+                    // eslint-disable-next-line max-len
+                    className="w-full h-32 border-cal-poly-gold border-4 bg-white flex justify-between items-center text-cal-poly-green cursor-pointer"
+                    style={{ borderRadius: "1.5rem" }}
+                >
+                    <h3 className="text-3xl font-medium pl-3">
+                        {teacher?.lastName}, {teacher?.firstName}
+                    </h3>
+                    <div className="text-right text-xl pr-3 font-medium flex-shrink-0">
+                        <div>{teacher?.department}</div>
+                        <div className="flex items-center justify-end">
+                            <img className="pr-1 h-4" src={star} alt="" />
+                            <div>{teacher?.overallRating}</div>
+                        </div>
+                        <div>{teacher?.numEvals} evals</div>
                     </div>
-                    <div>{teacher?.numEvals} evals</div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 }
