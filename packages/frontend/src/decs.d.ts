78dc0c707d2e91a75d754919c2951b7718cee9b6
Max Fisher <maxmfishernj@gmail.com>
2022-03-16 22:33:03 -0700

    revert to es modules for virtualized scrolling

--- packages/frontend/src/decs.d.ts
+++ packages/frontend/src/decs.d.ts
@@ -1,6 +1,6 @@
-declare module "fish-react-virtualized/dist/es/WindowScroller" {
+declare module "fish-react-virtualized/dist/commonjs/WindowScroller" {
     export * from "react-virtualized";
 }
-declare module "fish-react-virtualized/dist/es/List" {
+declare module "fish-react-virtualized/dist/commonjs/List" {
     export * from "react-virtualized";
 }
