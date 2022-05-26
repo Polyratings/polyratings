afdd9b3334b676e8a4728eed9ac6261f89d26467
mfish33 <maxmfishernj@gmail.com>
2022-05-26 10:17:07 -0700

    report action now button and labeled

--- packages/frontend/src/components/ClassSection.tsx
+++ packages/frontend/src/components/ClassSection.tsx
@@ -94,19 +94,20 @@ function ReportButton({ professorId, ratingId }: ReportButtonProps) {
                     </div>
                 </Backdrop>
             )}
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 m-auto mt-1 text-gray-500 hover:text-red-500 transition-all cursor-pointer"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                onClick={() => setFormShown(true)}
-            >
-                <path
-                    fillRule="evenodd"
-                    d="M3 6a3 3 0 013-3h10a1 1 0 01.8 1.6L14.25 8l2.55 3.4A1 1 0 0116 13H6a1 1 0 00-1 1v3a1 1 0 11-2 0V6z"
-                    clipRule="evenodd"
-                />
-            </svg>
+            <button type="button" title="Report Rating" onClick={() => setFormShown(true)}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 m-auto mt-1 text-gray-500 hover:text-red-500 transition-all cursor-pointer"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M3 6a3 3 0 013-3h10a1 1 0 01.8 1.6L14.25 8l2.55 3.4A1 1 0 0116 13H6a1 1 0 00-1 1v3a1 1 0 11-2 0V6z"
+                        clipRule="evenodd"
+                    />
+                </svg>
+            </button>
         </div>
     );
 }
