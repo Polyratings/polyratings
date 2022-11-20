0ccca68cd0e257c993cc51e20e2e1dd148404171
mfish33 <maxmfishernj@gmail.com>
2022-11-20 18:04:49 -0500

    update trpc package

--- packages/frontend/src/trpc.ts
+++ packages/frontend/src/trpc.ts
@@ -1,4 +1,4 @@
-import { createTRPCReact } from "@trpc/react";
+import { createTRPCReact } from "@trpc/react-query";
 
 // eslint-disable-next-line no-restricted-imports, import/no-relative-packages
 import type { AppRouter } from "@backend/index";
