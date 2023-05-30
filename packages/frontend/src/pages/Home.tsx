9f78bd5016276eb39f466a8bf6f168bfdf40502d
mfish33 <32677537+mfish33@users.noreply.github.com>
2023-05-29 21:42:02 -0700

    Accessibility Feature Showcase (#75)

--- packages/frontend/src/pages/Home.tsx
+++ packages/frontend/src/pages/Home.tsx
@@ -1,10 +1,10 @@
-/* eslint-disable react/no-array-index-key */
 import { inferProcedureOutput } from "@trpc/server";
 import { AppRouter } from "@backend/index";
 import { getRandomSubarray } from "@/utils";
 import homeHeader from "@/assets/home-header.webp";
 import homeCurveTransition from "@/assets/home-curve-transition.svg";
-import star from "@/assets/star.svg";
+import homeTags from "@/assets/home-tags.svg";
+import homeProfessorSummary from "@/assets/home-professor-summary.svg";
 import worstOfWorstBackground from "@/assets/worst-of-worst-background.webp";
 import { SearchBar, ProfessorCard } from "@/components";
 import { trpc } from "@/trpc";
@@ -12,7 +12,6 @@ import { trpc } from "@/trpc";
 export function Home() {
     const { data: allProfessors } = trpc.professors.all.useQuery();
 
-    const highlightedProfessor = getBestProfessors(allProfessors ?? [])?.[0];
     const bestOfTheBest = allProfessors ? getBestProfessors(allProfessors) : [];
 
     return (
@@ -24,7 +23,7 @@ export function Home() {
                     backgroundPosition: "center",
                     backgroundSize: "cover",
                 }}
-                className="relative h-screenWoNav lg:h-screen3/5 min-h-[30rem] "
+                className="relative h-screenWoNav lg:h-screen3/5 min-h-[30rem]"
             >
                 <div className="flex flex-col w-full h-2/3 lg:h-80 justify-center justify-items-center">
                     <h1 className="text-6xl md:text-9xl text-white text-center font-semibold drop-shadow-lg">
@@ -34,39 +33,38 @@ export function Home() {
                         <SearchBar showOnlyInput />
                     </div>
                 </div>
-                {/* Use -1 to make sure background image does not shine through bottom */}
                 <img
                     src={homeCurveTransition}
                     alt="curve transition"
-                    className="absolute left-0 -bottom-1 w-full lg:block hidden select-none pointer-events-none"
+                    className="absolute left-0 -bottom-7 w-full lg:block hidden select-none pointer-events-none"
                 />
             </div>
-            <div className="justify-center pl-5 lg:flex hidden z-10 relative">
-                <div className="w-1/2 transform xl:-translate-y-8 translate-y-2">
-                    <h2 className="xl:text-8xl lg:text-7xl font-semibold">Course Accessibility!</h2>
-                    {/* pl-2 since the title font has spacing with the H */}
-                    <p className="xl:w-2/3 lg:w-4/5 lg:text-2xl text-xl mt-8 font-medium pl-2">
+            <div className="lg:block hidden">
+                <div className="w-[60rem] text-center m-auto z-10 relative">
+                    <p className="absolute text-6xl -left-14 hidden xl:block">🎉</p>
+                    <p className="absolute text-6xl -right-14 hidden xl:block">🎉</p>
+                    <h3 className="text-5xl font-bold mb-8">
+                        Newest Feature: Course Accessibility!
+                    </h3>
+                    <p className="text-2xl font-medium mb-6">
                         We are happy to announce the release of the course accessibility allowing
-                        students to add tags to ratings. Top tags will be displayed at the top of
-                        the professor page.
+                        students to add tags to ratings.
                     </p>
                 </div>
-
-                <div
-                    className="bg-cal-poly-green h-80 rounded-3xl border-8 border-black flex flex-col justify-center items-center"
-                    style={{ width: "35rem" }}
-                >
-                    <h3 className="text-white text-5xl font-semibold text-center">
-                        Featured Professor
-                    </h3>
-                    <div className="flex mt-2 mb-6">
-                        {/* Nothing Unique about items  */}
-                        {[...Array(12)].map((_, i) => (
-                            <img key={i} src={star} className="h-7 w-7" alt="star" />
-                        ))}
-                    </div>
-                    <div className="w-11/12">
-                        <ProfessorCard professor={highlightedProfessor} />
+                <div className="flex justify-center items-start gap-16">
+                    <img
+                        className="w-[35rem]"
+                        src={homeTags}
+                        alt="Accessability tag form to rate professors"
+                    />
+                    <div className="text-left w-[31rem]">
+                        <img src={homeProfessorSummary} alt="Professor summary with tags" />
+                        <ul className="text-xl list-[home-list] mt-4">
+                            <li className="ml-7 mb-4">Select tags in Evaluation form</li>
+                            <li className="ml-7">
+                                Top tags will be displayed at the top of the professor page
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -79,7 +77,7 @@ export function Home() {
                     minHeight: "100vh",
                     clipPath: "polygon(0 100%,0 10%,100% 0,100% 100%)",
                 }}
-                className="mt-8 xl:mt-16 lg:block hidden"
+                className="lg:block hidden"
             >
                 <h2 className="text-white font-semibold text-8xl xl:text-9xl text-center pt-40">
                     Best of the Best
