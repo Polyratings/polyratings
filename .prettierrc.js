d0217c572dc356ef671ac3c3daae38cb09568308
Chris <chris2fourlaw@gmail.com>
2023-06-07 10:24:56 -0700

    remove generator and fix prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,5 @@
 module.exports = {
-    plugins: [require("prettier-plugin-tailwindcss")],
     pluginSearchDirs: false,
-    tailwindConfig: "./packages/frontend/tailwind.config.js",
     printWidth: 100,
     trailingComma: "all",
     useTabs: false,
