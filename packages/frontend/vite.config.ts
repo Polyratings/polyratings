e630339a72c2ca62e53905f9cea501234b874cb5
Chris <chris2fourlaw@gmail.com>
2023-07-31 19:13:10 -0700

    bump versions

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { resolve } from "path";
 import { visualizer } from "rollup-plugin-visualizer";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,12 +19,7 @@ export default defineConfig({
         host: "localhost",
     },
 
-    plugins: [
-        react(),
-        tsconfigPaths({
-            root: "../../",
-        }),
-    ],
+    plugins: [react(), tsconfigPaths()],
     build: {
         sourcemap: true,
         rollupOptions: {
